894a7dc25182311332b6043504bcc52d19a4d364
jmgodino <jmgodino@github.com>
2023-12-09 19:45:48 +0100

    soporte para DOM, JAXB, String y compartiva de tiempos

--- src/test/java/com/picoto/test/TestValidator.java
+++ src/test/java/com/picoto/test/TestValidator.java
@@ -14,6 +14,8 @@ import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stax.StAXSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
@@ -21,9 +23,24 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
+import com.picoto.Facturae;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.util.JAXBResult;
+
 public class TestValidator {
+
+	private JAXBContext ctx;
+	private long ini, fin;
+
+	public TestValidator() throws JAXBException {
+		super();
+		ctx = JAXBContext.newInstance("com.picoto");
+	}
 
 	private Validator initValidator(String xsdPath) throws SAXException, IOException {
 		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
@@ -33,52 +50,134 @@ public class TestValidator {
 	}
 
 	private InputStream getFile(String location) throws IOException {
-
 		return new FileInputStream(location);
+	}
 
+	public void validarPorBloques() throws XMLStreamException, SAXException, IOException, TransformerException {
+		XMLStreamReader reader = getStaxReader(getFile("./ejemplo.xml"));
+		Validator validator = initValidator("./src/main/xsd/Remesas.xml");
+
+		initTimeCalculation();
+		while (reader.hasNext()) {
+			if (reader.isStartElement()) {
+				if (isNodoFactura(reader)) {
+					String nodo = getStringCompleto(reader);
+					// Procesar el String al gusto
+					validator.validate(getStaxSource(nodo));
+				}
+			}
+			reader.next();
+		}
+		endTimeCalculation("String");
+	}
+
+	private boolean isNodoFactura(XMLStreamReader reader) {
+		return reader.getName().getLocalPart().equals("Facturae");
+	}
+
+	public void validarPorBloquesConDomAuxiliar()
+			throws XMLStreamException, SAXException, IOException, TransformerException {
+		XMLStreamReader reader = getStaxReader(getFile("./ejemplo.xml"));
+		Validator validator = initValidator("./src/main/xsd/Remesas.xml");
+
+		initTimeCalculation();
+		while (reader.hasNext()) {
+			if (reader.isStartElement()) {
+				if (isNodoFactura(reader)) {
+					Node node = getNodoCompleto(reader);
+					// Procesar al gusto
+					validator.validate(new DOMSource(node));
+				}
+			}
+			reader.next();
+		}
+		endTimeCalculation("DOM");
+	}
+
+	public void validarPorBloquesConJaxbAuxiliar()
+			throws XMLStreamException, SAXException, IOException, TransformerException, JAXBException {
+		XMLStreamReader reader = getStaxReader(getFile("./ejemplo.xml"));
+
+		initTimeCalculation();
+		while (reader.hasNext()) {
+			if (reader.isStartElement()) {
+				if (isNodoFactura(reader)) {
+					Facturae face = getObjetoCompleto(reader);
+					// Aqui ya se ha valida al montar el JAX-B no hace falta validar
+				}
+			}
+			reader.next();
+		}
+		endTimeCalculation("JAXB");
+	}
+
+	public void validarCompleto() throws XMLStreamException, SAXException, IOException {
+
+		XMLStreamReader reader = getStaxReader(getFile("./ejemplo.xml"));
+		Validator validator = initValidator("./src/main/xsd/Remesas.xml");
+
+		long ini = System.currentTimeMillis();
+		validator.validate(new StAXSource(reader));
+
+		long fin = System.currentTimeMillis();
+		debug("Tiempo total validación única: " + (fin - ini) + "ms");
+
+	}
+
+	private static String getStringCompleto(XMLStreamReader reader) throws XMLStreamException, TransformerException {
+		StringWriter sw = new StringWriter();
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer t = tf.newTransformer();
+		t.transform(new StAXSource(reader), new StreamResult(sw));
+		return sw.toString();
+	}
+
+	private static Node getNodoCompleto(XMLStreamReader reader) throws XMLStreamException, TransformerException {
+		DOMResult dr = new DOMResult();
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer t = tf.newTransformer();
+		t.transform(new StAXSource(reader), dr);
+		return dr.getNode();
+	}
+
+	private Facturae getObjetoCompleto(XMLStreamReader reader) throws JAXBException, TransformerException {
+		JAXBResult jaxbr = new JAXBResult(ctx);
+
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer t = tf.newTransformer();
+		t.transform(new StAXSource(reader), jaxbr);
+		return (Facturae) jaxbr.getResult();
+	}
+
+	private XMLStreamReader getStaxReader(InputStream is) throws XMLStreamException {
+		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+		return xmlInputFactory.createXMLStreamReader(is);
+	}
+
+	private StAXSource getStaxSource(String xml) throws XMLStreamException {
+		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+		return new StAXSource(xmlInputFactory.createXMLStreamReader(new StringReader(xml)));
+	}
+
+	public void debug(String msg) {
+		System.out.println(msg);
+	}
+
+	public void initTimeCalculation() {
+		ini = System.currentTimeMillis();
+	}
+
+	public void endTimeCalculation(String tipo) {
+		fin = System.currentTimeMillis();
+		debug(String.format("Tiempo total validación %s por bloques:%d ms", tipo, fin-ini));
 	}
 
 	public static void main(String args[]) throws Exception {
 		TestValidator tv = new TestValidator();
-
-		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-		XMLStreamReader reader1 = xmlInputFactory.createXMLStreamReader(new FileInputStream("./ejemplo.xml"));
-
-		Validator validator1 = tv.initValidator("./src/main/xsd/Facturaev322.xml");
-		long ini = System.currentTimeMillis();
-		while (reader1.hasNext()) {
-			if (reader1.isStartElement()) {
-				if (reader1.getName().getLocalPart().equals("Facturae")) {
-					String nodo = imprimirNodoCompleto(reader1);
-					System.out.println(nodo);
-					XMLStreamReader readerAux = xmlInputFactory.createXMLStreamReader(new StringReader(nodo));
-					validator1.validate(new StAXSource(readerAux));
-					//validator1.validate(new StAXSource(reader1));
-					//System.out.println("validado correctamente");
-				}
-			}
-			reader1.next();
-		}
-		long fin = System.currentTimeMillis();
-		System.out.println("Tiempo total validación por bloques: " + (fin - ini) + "ms");
-
-		Validator validator2 = tv.initValidator("./src/main/xsd/Remesas.xml");
-		XMLStreamReader reader2 = xmlInputFactory.createXMLStreamReader(new FileInputStream("./ejemplo.xml"));
-
-		ini = System.currentTimeMillis();
-		validator2.validate(new StAXSource(reader2));
-
-		fin = System.currentTimeMillis();
-		System.out.println("Tiempo total validación única: " + (fin - ini) + "ms");
-
-	}
-
-	private static String imprimirNodoCompleto(XMLStreamReader reader) throws XMLStreamException, TransformerException {
-		 StringWriter sw = new StringWriter();
-		 TransformerFactory tf = TransformerFactory.newInstance();
-         Transformer t = tf.newTransformer();
-         t.transform(new StAXSource(reader), new StreamResult(sw));
-		 return sw.toString();
+		tv.validarCompleto();
+		tv.validarPorBloques();
+		tv.validarPorBloquesConDomAuxiliar();
+		tv.validarPorBloquesConJaxbAuxiliar();
 	}
 
 }
