2e24d94b2dc4ec387a0c599718394a0291ffcbbe
jmgodino <jmgodino@local.com>
2024-01-03 10:16:02 +0100

    ajustes firma

--- src/main/java/com/picoto/main/xades4j/SignerTest.java
+++ src/main/java/com/picoto/main/xades4j/SignerTest.java
@@ -1,10 +1,12 @@
 package com.picoto.main.xades4j;
 
+import org.apache.xml.security.signature.XMLSignature;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import xades4j.production.BasicSignatureOptions;
 import xades4j.production.Enveloped;
+import xades4j.production.SignatureAlgorithms;
 import xades4j.production.XadesBesSigningProfile;
 import xades4j.production.XadesSigner;
 
@@ -15,7 +17,10 @@ public class SignerTest extends SignerBase {
 		Element elemToSign = doc.getDocumentElement();
 
 		XadesSigner signer = new XadesBesSigningProfile(keyingProviderMy)
-				.withBasicSignatureOptions(new BasicSignatureOptions().includePublicKey(true)).newSigner();
+				.withBasicSignatureOptions(new BasicSignatureOptions().includePublicKey(true))
+				.withSignatureAlgorithms(new SignatureAlgorithms().withSignatureAlgorithm("RSA",
+						XMLSignature.ALGO_ID_SIGNATURE_RSA_SHA512))
+				.newSigner();
 		new Enveloped(signer).sign(elemToSign);
 		outputDocument(doc, "./examples/ejemplo-signed-xades4j.xml");
 	}
